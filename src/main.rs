3be015e8d553df9dbb0ee9688fcf3b148fec8437
Karl Schomerus <karl.schomerus2@gmail.com>
2022-07-30 19:43:57 +0200

    library crate

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,11 @@
+use hj::Config;
 use clap::Parser;
 
 
-#[derive(Parser, Debug)]
-#[clap(name = "hj compiler")]
-#[clap(author = "karl2883 & Trubiso")]
-#[clap(version = "0.0")]
-#[clap(about = "Compiles hj source files into executables")]
-struct CliArgs {
-    /// The name of the output file
-    #[clap(short, long)]
-    output: Option<String>,
-
-    /// Print debug information
-    #[clap(short, long)]
-    debug: bool,
-
-    /// The name of the file to be compiled
-    file: String,
-}
-
 fn main() {
-    let args = CliArgs::parse();
+    let args = Config::parse();
 
-    println!("Compiling  file {}!", args.file);
-    if args.debug {
-        println!("Printing debug information!");
-    }
-    if let Some(output_file) = args.output {
-        println!("Outputting to file {output_file}!");
+    if let Err(()) = hj::run(args) {
+        println!("Unable to compile (see errors above)!");
     }
 }
